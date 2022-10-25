12a92f973427dba928337b38522cdfcbb2b0844c
SteevoMcUltraCool <stephen.radice23@sitechhs.com>
2022-10-25 14:02:50 -0400

    2332

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const DOM = {
   spontButtons: document.getElementsByName("spontaneous"),
   helpfulButtons: document.getElementsByName("helpful"),
 
-  hairTrue: document.getElementById(hairTrue),
-  gpaTrue: document.getElementById(gpaTrue),
-  bmiTrue: document.getElementById(bmiTrue),
+  hairTrue: document.getElementById("hairTrue"),
+  gpaTrue: document.getElementById("gpaTrue"),
+  bmiTrue: document.getElementById("bmiTrue"),
+
+  rr: document.getElementById("rr")
 };
 
 function getRadioButtonValue(buttons) {
   for (i = 0; i < buttons.length, (i = i + 1); ) {
-    if (buttons[i].checked) {
+    if (buttons[i].checked); {
+      console.warn(buttons[i])
       return buttons[i].value;
     }
   }
@@ -22,6 +25,7 @@ function getRadioButtonValue(buttons) {
 let plants = {
   Sunflower: {
     name: "Sunflower",
+    height: 65,
     secondChances: 4,
     spontaneous: 1,
     helpful: 5,
@@ -32,6 +36,7 @@ let plants = {
   },
   Peashooter: {
     name: "Peashooter",
+    height: 70,
     secondChances: 2,
     spontaneous: 4,
     helpful: 3,
@@ -42,6 +47,7 @@ let plants = {
   },
   WallNut: {
     name: "Wall-Nut",
+    height: 69,
     secondChances: 3,
     spontaneous: 2,
     helpful: 4,
@@ -52,6 +58,7 @@ let plants = {
   },
   HypnoShroom: {
     name: "Hypno-Shroom",
+    height: 62,
     secondChances: 5,
     spontaneous: 4,
     helpful: 4,
@@ -62,6 +69,7 @@ let plants = {
   },
   PotatoMine: {
     name: "Potato-Mine",
+    height: 59,
     secondChances: 1,
     spontaneous: 5,
     helpful: 1,
@@ -72,6 +80,7 @@ let plants = {
   },
   BonkChoy: {
     name: "Bonk-Choy",
+    height: 74,
     secondChances: 2,
     spontaneous: 4,
     helpful: 2,
@@ -82,10 +91,42 @@ let plants = {
   },
 };
 
-function compareToPlant(plant, secondChances) {}
+function compareToPlant(plant, height,secondChances, spontaneous, helpful, hair, gpa, bmi) {
+  let heightDiff =  Math.min( Math.abs(height - plant.height), 10 )
+  let secondChanceDiff = Math.abs((secondChances- plant.secondChances) * 4.5)
+  let spontaneousDiff =Math.abs( (spontaneous- plant.spontaneous) * 4.5)
+  let helpfulDiff = Math.abs((helpful- plant.helpful) *4.5)
+  let hairDiff = 12, gpaDiff = 12, bmiDiff = 12
+  if (hair == plant.hair) {
+    hairDiff = 0
+  }
+  if (gpa == plant.gpa) {
+    gpaDiff = 0
+  }
+  if (bmi == plant.bmi) {
+    bmiDiff = 0
+  }
+  console.log(secondChances,secondChanceDiff)
+  return 100 - (heightDiff+secondChanceDiff+spontaneousDiff+helpfulDiff+hairDiff+gpaDiff+bmiDiff)
+}
 
 function fetchResults() {
+  let height = Number(DOM.enterHeight.value)
   let secondChances = getRadioButtonValue(DOM.chanceButtons);
+  let spontaneous = getRadioButtonValue(DOM.spontButtons);
+  let helpful = getRadioButtonValue(DOM.helpfulButtons);
+
+  let hair = DOM.hairTrue.checked
+  let gpa = DOM.gpaTrue.checked
+  let bmi = DOM.bmiTrue.checked
+  let closestScore = 0
+  let closestPlant = {}
   for (let k in plants) {
+    let c = compareToPlant(plants[k], height, secondChances,spontaneous,helpful, hair, gpa, bmi)
+    if (c>closestScore) {
+      closestScore = c
+      closestPlant = plants[k]
+    }
   }
+  DOM.rr.innerHTML = `You ara a : ${closestPlant.name} with a ${closestScore}% similarity!`
 }
